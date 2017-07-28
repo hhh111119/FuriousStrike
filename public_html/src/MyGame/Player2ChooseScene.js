e2e77c47515ed0b0346f979f11d62c7084efaf91
Xie JinChi <xie_jinchi@foxmail.com>
2017-07-28 08:57:45 +0800

    介绍界面

--- public_html/src/MyGame/Player2ChooseScene.js
+++ public_html/src/MyGame/Player2ChooseScene.js
@@ -34,12 +34,12 @@ Player2ChooseScene.prototype.initialize = function () {
     )
     this.mCamera.setBackgroundColor([1, 1, 1, 1])
     //character1
-  
+  5
     this.mCharacter1 = new  CharacterInfo()
     this.mCharacter1.setName('Hulk',3,[5,70])
     this.mCharacter1.setColor([1,0,0,1],[4,4],[6,65])
     this.mCharacter1.setWeight('150kg',2,[5,60])
-    this.mCharacter1.setSkill('Back off',2,[5,55])
+    this.mCharacter1.setSkill('BACK OFF',2,[5,55])
     this.mCharacter1.setSkillDetail('rotate',2,[5,53])
     this.mCharacter1.setFeature('power',2,[5,48])
     //character2
@@ -47,7 +47,7 @@ Player2ChooseScene.prototype.initialize = function () {
     this.mCharacter2.setName('Flash',2,[35,70])
     this.mCharacter2.setColor([0,0,0,1],[3,3],[36,65])
     this.mCharacter2.setWeight('70kg',2,[35,60])
-    this.mCharacter2.setSkill('Even Faster',2,[35,55])
+    this.mCharacter2.setSkill('EVEN FASTER',2,[35,55])
     this.mCharacter2.setSkillDetail('fastest speed',2,[35,53])
     this.mCharacter2.setFeature('quick',2,[35,48])
     //character3 
@@ -55,7 +55,7 @@ Player2ChooseScene.prototype.initialize = function () {
     this.mCharacter3.setName('CaptainAmerica',2,[65,70])
     this.mCharacter3.setColor([0,0,0,1],[3,3],[66,65])
     this.mCharacter3.setWeight('85kg',2,[65,60])
-    this.mCharacter3.setSkill('Puzzle',2,[65,55])
+    this.mCharacter3.setSkill('PUZZLE',2,[65,55])
     this.mCharacter3.setSkillDetail('reverse other control',2,[65,53])
     this.mCharacter3.setFeature('balance',2,[65,48])
     
